ea771ce972439c7776bed9ee8a0cec6257fe160e
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-10-09 20:18:58 -0300

    Criação de projeto spring com rabbitmq.

---
+++ spring-rabbit-producer/src/main/java/com/github/marciokleber/springrabbitproducer/SpringRabbitProducerApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.springrabbitproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitProducerApplication.class, args);
+	}
+
+}
