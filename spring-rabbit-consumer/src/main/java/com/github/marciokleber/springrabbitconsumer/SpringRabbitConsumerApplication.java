12d4e2363c028ee83f2e5f7334c434053237720b
Marcio Kleber Junior Lopes de Souza <marciokjls@gmail.com>
2024-10-09 20:21:34 -0300

    criação do esqueleto do consumer

---
+++ spring-rabbit-consumer/src/main/java/com/github/marciokleber/springrabbitconsumer/SpringRabbitConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.github.marciokleber.springrabbitconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitConsumerApplication.class, args);
+	}
+
+}
